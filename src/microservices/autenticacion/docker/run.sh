16c1855d2eebb3c9cfdb898697a1b250464e8bd7
Gabriel Reus Rodriguez <gabrielin@gmail.com>
2025-12-22 21:47:48 +0100

    Comentario para mañana , docker run --env-file

--- src/microservices/autenticacion/docker/run.sh
+++ src/microservices/autenticacion/docker/run.sh
@@ -1,4 +1,5 @@
 #!/bin/env bash
 
 # -d es para que se ejecute en background , dettached.
+# --env-file .env es para especificarle el fichero de variables de environmnet a docker.
 docker run -p 2203:2203 uoc-autenticacion
