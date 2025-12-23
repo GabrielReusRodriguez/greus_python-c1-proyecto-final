985a2e136d81b16f5d3001d6446fbd1de7f53af8
Gabriel Reus Rodriguez <gabrielin@gmail.com>
2025-12-23 20:33:40 +0100

    Iniciamos el cliente y cambios menores

--- bin/tests/test_authentication_check.sh
+++ bin/tests/test_authentication_check.sh
@@ -3,4 +3,5 @@
 echo -e "\tTesteando el microsevicio de Autenticacion Check de token JWT"
 
 echo -e "\t\t Check jwt  **********************"
-curl --get -H "Authorization:Bearer $1" http://localhost:2203/auth/check
+curl --get -H "Authorization:Bearer $1" http://localhost:2203/auth/check?rol=usuario
+curl --get -H "Authorization:Bearer $1" http://localhost:2203/auth/check?rol=admin
