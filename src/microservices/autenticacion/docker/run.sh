f0091f420b2e7e8b57dd453ae5ced1ed976ac226
Gabriel Reus <Gabriel.Reus@gmail.com>
2025-12-25 22:31:43 +0100

    El contenedor se ejecuta en background

--- src/microservices/autenticacion/docker/run.sh
+++ src/microservices/autenticacion/docker/run.sh
@@ -2,4 +2,4 @@
 
 # -d es para que se ejecute en background , dettached.
 # --env-file .env es para especificarle el fichero de variables de environmnet a docker.
-docker run  --env-file ./.env -p 2203:2203 uoc-autenticacion
+docker run  --env-file ./.env -d -p 2203:2203 uoc-autenticacion
