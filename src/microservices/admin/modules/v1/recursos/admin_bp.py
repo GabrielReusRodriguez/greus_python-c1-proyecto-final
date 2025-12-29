3b7a9a8f8a1425ba992ad6c6620bb32797354723
Gabriel Reus Rodriguez <gabrielin@gmail.com>
2025-12-29 15:40:34 +0100

    Finalizado el admin_bp , pdte testear

--- src/microservices/admin/modules/v1/recursos/admin_bp.py
+++ src/microservices/admin/modules/v1/recursos/admin_bp.py
@@ -114,7 +114,7 @@ def consulta_usuario(id:int):
     
 
 @admin_v1_bp.route('/doctores', methods=['POST'])
-@require_rol('admin')
+@require_rol(['admin'])
 def create_doctor():
     # Obtengo el token jwt
     auth_header = request.headers.get('Authorization')
@@ -144,45 +144,175 @@ def create_doctor():
 @admin_v1_bp.route('/doctores', methods=['GET'])
 @require_rol(['admin'])
 def consulta_doctores():
-    pass
+    # Devuelvo la lista de doctores
+    doctores = []
+    pagina = request.args.get('pagina')
+    if pagina is None:
+        pagina = 0 
+    i = 0
+    inicio_pagina = ITEMS_POR_PAGINA * pagina
+    # Select de los drs.
+    results = db.session.query(Doctor).all()
+    for result in results:
+        if i >= inicio_pagina:
+            # creo el json del dr.
+            doctores.append(result.to_dict())
+        if len(doctores) > ITEMS_POR_PAGINA:
+            break
+        i = i + 1
+    return jsonify({'msg' : 'OK', 'payload' : doctores}), 200, {'Content-type' : 'application/json'}
+
+    """
+    i = 0
+    inicio_pagina = ITEMS_POR_PAGINA * pagina
+    users = []
+    #Itero usuario por usuario buscando roles admin y secretario.
+    for user in data['payload']:
+        if user['rol'] == 'admin' or user['rol'] == 'secretario':
+            if i >= inicio_pagina:
+                # Borro el password ( por seguridad)
+                user['password'] = ''
+                users.append(user)
+            i = i + 1
+        if len(users) > ITEMS_POR_PAGINA:
+            break
+    return jsonify({'msg' : 'OK', 'payload' : users}), 200, {'Content-type' : 'application/json'}
+    """
 
 @admin_v1_bp.route('/doctores/<int:id>', methods=['GET'])
 @require_rol(['admin'])
 def consulta_doctores(id : int):
-    pass
-
+    # Devuelvo el Doctor por id de doctor.
+    dr = db.session.query(Doctor).filter(Doctor.id_doctor == id).first()
+    if dr is None:
+        return jsonify({'msg' : 'OK'}), 404, {'Content-type' : 'application/json'}
+    return jsonify({'msg' : 'OK' , 'payload' : dr.to_dict()}), 200, {'Content-type' : 'application/json'}
 
 
 @admin_v1_bp.route('/pacientes', methods=['POST'])
 @require_rol(['admin'])
 def create_paciente():
-    pass
+    """
+        Paciente
+        Datos mínimos del paciente:
+
+        id_paciente (PK)
+        id_usuario (FK opcional)
+        nombre
+        teléfono
+        estado(ACTIVO/INACTIVO)
+    """
+    # Obtengo el token jwt
+    auth_header = request.headers.get('Authorization')
+    if auth_header is None:
+        return jsonify({'msg': 'No hemos recibido el jwt'}), 403, {'Content-type' : 'application/json'}
+    # Obtenemos los parámetros json.
+    data = request.get_json()
+    # Check... solo checqueo el rol ya que el resto se comprobará en el microservicio de autenticacion.
+    if data is None:
+        return jsonify({'msg' : 'No hemos recibido parámetros'}), 401, {'Content-type' : 'application/json'}
+    if data.get('telefono') is None:
+        return jsonify({'msg' : 'No hemos recibido el teléfono'}), 401, {'Content-type' : 'application/json'}
+    if data.get('nombre') is None:
+        return jsonify({'msg': 'No hemos recibido el nombre'}), 401, {'Content-type' : 'application/json'}
+    if data.get('estado') is None:
+        return jsonify({'msg': 'No hemos recibido el estado'}), 401, {'Content-type' : 'application/json'}
+    response = requests.post(url=AUTH_MICROSERVICE_URL + "create_user", json = data, headers= {'Authorization' : auth_header})
+    if response.status_code != 200:
+        return response.json(), response.status_code, {'Content-type' : 'application/json'}
+    # Tenemos el user creado, ahopra creamos los datos del doctor.
+    paciente = Paciente(nombre= data.get('nombre'), teléfono= data.get('telefono'),estado = data.get('estado'), id_usuario  = response.json()['payload']['id_usuario'])
+    db.session.add(paciente)
+    db.session.commit()
+    # Si ha llegado hasta aqui, significa que el usuario se ha creado bien por lo que seguimos creando el tipo de usuario = medico, admin...
+    return jsonify({'msg' : 'OK', 'payload' : paciente.to_dict()}), 200, {'Content-type' : 'application/json'}
 
 
 @admin_v1_bp.route('/pacientes?<int:pagina>', methods=['GET'])
+@require_rol(['admin'])
 def consulta_pacientes(pagina : int):
     # Recuerda paginacion!!!
-    pass
+    pacientes = []
+    pagina = request.args.get('pagina')
+    if pagina is None:
+        pagina = 0 
+    i = 0
+    inicio_pagina = ITEMS_POR_PAGINA * pagina
+    results = db.session.query(Paciente).all()
+    for result in results:
+        if i >= inicio_pagina:
+            # Añado el paciente
+            pacientes.append(result.to_dict())
+        if len(pacientes) > ITEMS_POR_PAGINA:
+            break
+        i = i + 1
+    return jsonify({'msg' : 'OK', 'payload' : pacientes}), 200, {'Content-type' : 'application/json'}
 
 @admin_v1_bp.route('/pacientes/<int:id>', methods=['GET'])
+@require_rol(['admin'])
 def consulta_paciente(id : int):
-    pass
+    # Obtenemos un paciente.
+    paciente = db.session.query(Paciente).filter(Paciente.id_paciente == id).first()
+    if paciente is None:
+        return jsonify({'msg': 'OK'}),404,{'Content-type' : 'application/json'}
+    return jsonify({'msg' : 'OK' , 'payload' : paciente.to_dict()}), 200, {'Content-type': 'application/json'}
 
 
 @admin_v1_bp.route('/centros',methods=['POST'])
 @require_rol(['admin'])
 def create_centro():
-    pass
+    """
+    Centro Médico
+        id_centro (PK)
+        nombre
+        direccion
+    """
+    # Creo un centro.
+    # Obtenemos los parámetros json.
+    data = request.get_json()
+    # Check... solo checqueo el rol ya que el resto se comprobará en el microservicio de autenticacion.
+    if data is None:
+        return jsonify({'msg' : 'No hemos recibido parámetros'}), 401, {'Content-type' : 'application/json'}
+    if data.get('direccion') is None:
+        return jsonify({'msg' : 'No hemos recibido la direccion'}), 401, {'Content-type' : 'application/json'}
+    if data.get('nombre') is None:
+        return jsonify({'msg': 'No hemos recibido el nombre'}), 401, {'Content-type' : 'application/json'}
+
+    # Creamos el centro...
+    centro = CentroMedico(nombre= data.get('nombre'), direccion = data.get('direccion'))
+    db.session.add(centro)
+    db.session.commit()
+    # Si ha llegado hasta aqui, significa que el usuario se ha creado bien por lo que seguimos creando el tipo de usuario = medico, admin...
+    return jsonify({'msg' : 'OK', 'payload' : centro.to_dict()}), 200, {'Content-type' : 'application/json'}
+    
 
 
 @admin_v1_bp.route('/centros?<int:pagina>', methods=['GET'])
 def consulta_centros(pagina : int):
-    # Recuerda paginacion!!!
-    pass
+    centros = []
+    pagina = request.args.get('pagina')
+    if pagina is None:
+        pagina = 0 
+    i = 0
+    inicio_pagina = ITEMS_POR_PAGINA * pagina
+    results = db.session.query(CentroMedico).all()
+    for result in results:
+        if i >= inicio_pagina:
+            # Añado el paciente
+            centros.append(result.to_dict())
+        if len(centros) > ITEMS_POR_PAGINA:
+            break
+        i = i + 1
+    return jsonify({'msg' : 'OK', 'payload' : centros}), 200, {'Content-type' : 'application/json'}
+
 
 @admin_v1_bp.route('/centro/<int:id>', methods=['GET'])
 def consulta_centro(id : int):
-    pass
+    # Obtenemos un paciente.
+    centro = db.session.query(CentroMedico).filter(CentroMedico.id_centro == id).first()
+    if centro is None:
+        return jsonify({'msg': 'OK'}),404,{'Content-type' : 'application/json'}
+    return jsonify({'msg' : 'OK' , 'payload' : centro.to_dict()}), 200, {'Content-type': 'application/json'}
 
 
 
