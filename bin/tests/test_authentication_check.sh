ef603b1d28670f4413e52e3b7c63fb73e940808a
Gabriel Reus Rodriguez <gabrielin@gmail.com>
2026-01-12 21:41:19 +0100

    Eliminados scripts sobrantes

--- bin/tests/test_authentication_check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env bash
-
-echo -e "\tTesteando el microsevicio de Autenticacion Check de token JWT"
-
-echo -e "\t\t Check jwt  **********************"
-curl --get -H "Authorization:Bearer $1" http://localhost:2203/auth/check?rol=usuario
-curl --get -H "Authorization:Bearer $1" http://localhost:2203/auth/check?rol=admin
--- bin/tests/test_authentication_usuarios.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/env bash
-
-curl --get -H  "Authorization: Bearer $1" http://localhost:2203/auth/usuarios
